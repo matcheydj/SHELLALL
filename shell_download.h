6f50cc710c87e6925c604225abd2a46c602a1813
matcheydj <matcheydj@users.noreply.github.com>
2023-12-10 18:47:24 -0600

    Create shell_download.h

---
+++ shell_download.h
@@ -0,0 +1,13 @@
+#ifndef SHELL_DOWNLOAD_H
+#define SHELL_DOWNLOAD_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+// Function to download a shell
+void download_shell(char *shell_name);
+
+// Function to extract a shell
+void extract_shell(char *archive_filename);
+
+#endif // SHELL_DOWNLOAD_H
