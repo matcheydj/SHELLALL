871f3de82b512e004bd736180f85f15d204e8221
matcheydj <matcheydj@users.noreply.github.com>
2023-12-10 18:55:02 -0600

    Update shell_download.h

--- shell_download.h
+++ shell_download.h
@@ -3,11 +3,20 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <curl/curl.h>
+#include <openssl/md5.h>
 
 // Function to download a shell
-void download_shell(char *shell_name);
+int download_shell(char *shell_name, char *url);
 
 // Function to extract a shell
-void extract_shell(char *archive_filename);
+int extract_shell(char *archive_filename);
+
+// Function to handle cellular handover
+int handle_cellular_handover(CURL *curl);
+
+// Function to verify download integrity with checksum
+int verify_download_integrity(char *filename, char *expected_checksum);
 
 #endif // SHELL_DOWNLOAD_H
